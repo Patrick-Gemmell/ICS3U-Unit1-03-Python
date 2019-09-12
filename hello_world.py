7e48607c0a4cef4e3588d53dd45474f0c61fa6b2
Ubuntu <ubuntu@ip-172-31-93-49.ec2.internal>
2019-09-12 17:04:52 +0000

    commit added hello world python

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Patrick Gemmell
+# Created on: September 12 2019
+# This program runs Hello World on pyhton
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
